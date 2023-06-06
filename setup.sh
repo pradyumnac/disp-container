9326444a5d1ad28cf21a9de9c437b6feb2e0d0c4
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2023-06-06 23:41:32 +0530

    Add kind install for linux and mac. Add kind create cluster comman

--- setup.sh
+++ setup.sh
@@ -1,7 +1,39 @@
-##!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# Install Kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.19.0/kind-darwin-arm64 && chmod +x ./kind-darwin-arm64 && sudo mv ./kind-darwin-arm64 ~/.local/bin/kind
+KIND_VERSION=0.19.0
+
+# Install Kind if not installed
+if ! command -v kind &> /dev/null
+then
+  echo "Installing Kind"
+  # Check linux architecture
+  ARCH=$(uname)
+
+  case $ARCH in
+      "Darwin")
+          echo "MacOS"
+          # for m1 chip
+          [ $(uname -m) = arm64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-darwin-arm64 && chmod +x ./kind-darwin-arm64 && sudo mv ./kind-darwin-arm64 ~/.local/bin/kind
+
+          # For Intel Macs
+          [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-darwin-amd64 && chmod +x ./kind-darwin-amd64 && sudo mv ./kind-darwin-amd64 ~/.local/bin/kind
+          ;;
+      "Linux")
+          echo "Linux Installation for kind $KIND_VERSION"
+          # For AMD64 / x86_64
+          [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-linux-amd64 && chmod +x ./kind-linux-amd64 && sudo mv ./kind-linux-amd64 ~/.local/bin/kind
+          # For ARM64
+          [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-linux-arm64 && chmod +x ./kind-linux-arm64 && sudo mv ./kind-linux-arm64 ~/.local/bin/kind
+          ;;
+      *)
+          echo "Unsupported OS"
+          exit 1
+          ;;
+  esac
+fi
+
+# Start kind cluster
+kind create cluster --name helm --image kindest/node:v1.26.0
 
 # Build disposable dev container(Docker)
 docker build -t dev-container-prady -f Dockerfile .
